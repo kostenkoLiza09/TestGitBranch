1bd2d16717151c6823b6d083b2dfa23722d48e6f
kostenkoLiza09 <kostaz063@gmail.com>
2025-04-09 15:35:37 +0200

    add step7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -54,7 +54,13 @@ public class Main {
                 .sorted()
                 .collect(Collectors.toList());
         System.out.println( "Step6:" + step6);
+
+        Integer step7 = list.stream()
+                .max(Integer::compareTo)
+                .orElse(null);
+        System.out.println("Step7: " + step7);
     }
+
 
     public static void readFile (String fileName) throws IOException {
         Path path = Path.of(fileName);
