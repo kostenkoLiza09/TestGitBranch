c53285d169cf86262825a9f789993fe35db6a890
kostenkoLiza09 <kostaz063@gmail.com>
2025-04-09 15:55:37 +0200

    add code to merge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -58,8 +58,9 @@ public class Main {
         Integer step7 = list.stream()
                 .max(Integer::compareTo)
                 .orElse(null);
-        System.out.println("Step7: " + step7);
+        System.out.println("Step: " + step7);
     }
+
 
 
     public static void readFile (String fileName) throws IOException {
